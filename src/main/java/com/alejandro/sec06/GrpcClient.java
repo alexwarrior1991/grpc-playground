fa5fc462e6f6b109d1bc0f8e527c73610ba96de4
alejandro <aguerreroegido@gmail.com>
2026-08-14 15:42:02 +0100

    Switch `GrpcClient` to use `BankServiceGrpc.newStub` for asynchronous account balance retrieval in `sec06` package.

--- src/main/java/com/alejandro/sec06/GrpcClient.java
+++ src/main/java/com/alejandro/sec06/GrpcClient.java
@@ -1,26 +1,47 @@
 package com.alejandro.sec06;
 
+import com.alejandro.models.sec06.AccountBalance;
 import com.alejandro.models.sec06.BalanceCheckRequest;
 import com.alejandro.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class GrpcClient {
 
     private static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
 
-    static void main() {
+    static void main() throws InterruptedException {
 
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
+        var stub = BankServiceGrpc.newStub(channel);
 
-        var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build());
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("{}", accountBalance);
+            }
 
-        log.info("{}", balance);
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("completed");
+            }
+        });
+
+        log.info("done");
+        Thread.sleep(Duration.ofSeconds(1));
+
 
     }
 }
