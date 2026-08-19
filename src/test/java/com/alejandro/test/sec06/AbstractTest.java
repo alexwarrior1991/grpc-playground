aae49c5f8cbd3b01a05d6f32a3494106849f788c
alejandro <aguerreroegido@gmail.com>
2026-08-19 11:51:10 +0100

    Add bidirectional streaming test `Lec05BiDirectionalStreamingTest` for `Transfer` method in `sec06` package with new stubs and response validation in `AbstractTest`

--- src/test/java/com/alejandro/test/sec06/AbstractTest.java
+++ src/test/java/com/alejandro/test/sec06/AbstractTest.java
@@ -2,22 +2,26 @@ package com.alejandro.test.sec06;
 
 import com.alejandro.common.GrpcServer;
 import com.alejandro.models.sec06.BankServiceGrpc;
+import com.alejandro.models.sec06.TransferServiceGrpc;
 import com.alejandro.sec06.BankService;
+import com.alejandro.sec06.TransferService;
 import com.alejandro.test.common.AbstractChannelTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public abstract class AbstractTest extends AbstractChannelTest {
 
-    private final GrpcServer grpcServer = GrpcServer.create(new BankService());
+    private final GrpcServer grpcServer = GrpcServer.create(new BankService(), new TransferService());
     protected BankServiceGrpc.BankServiceStub bankStub;
     protected BankServiceGrpc.BankServiceBlockingStub bankBlockingStub;
+    protected TransferServiceGrpc.TransferServiceStub transferStub;
 
     @BeforeAll
     public void setup(){
         this.grpcServer.start();
         this.bankBlockingStub = BankServiceGrpc.newBlockingStub(channel);
         this.bankStub = BankServiceGrpc.newStub(channel);
+        this.transferStub = TransferServiceGrpc.newStub(channel);
     }
 
     @AfterAll
